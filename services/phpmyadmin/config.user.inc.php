f00df1f84e60e6b0423366728580f5b6988f70b3
guiguoershao <guiguoershao@163.com>
2023-08-29 14:23:19 +0800

    加入 drupal10

--- services/phpmyadmin/config.user.inc.php
+++ services/phpmyadmin/config.user.inc.php
@@ -16,4 +16,19 @@ $i++; // server 2 :
 $cfg['Servers'][$i]['verbose']   = 'no2';
 $cfg['Servers'][$i]['host']      = 'mysql5';
 */
+$cfg['blowfish_secret']='multiServerExample70518';
 
+$i = 0;
+
+$i++; // server 1 :
+$cfg['Servers'][$i]['verbose']   = 'mysql-8.0';
+$cfg['Servers'][$i]['host']      = 'mysql';
+
+
+$i++; // server 2 :
+$cfg['Servers'][$i]['verbose']   = 'mysql5-5.7';
+$cfg['Servers'][$i]['host']      = 'mysql5';
+
+$cfg['ExecTimeLimit'] = 0;
+$cfg['MemoryLimit'] = 0;
+$memory_limit = 0;
